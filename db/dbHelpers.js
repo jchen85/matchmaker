58a086655471ca791cf190587875a90cc0019018
JC <jc@jcmbp.local>
2016-01-25 15:53:26 -0800

    created test files, added es6 and babel to package.json

--- db/dbHelpers.js
+++ db/dbHelpers.js
@@ -1,3 +1,5 @@
+import db from './config'
+
 ///////////////// DB helpers /////////////////////
 
 // get all users
---
+++ test/dbHelpers.js
@@ -0,0 +1,7 @@
+import {expect} from 'chai';
+
+describe('database helpers', () => {
+	describe('getRandomUsers', () => {
+
+	});
+});

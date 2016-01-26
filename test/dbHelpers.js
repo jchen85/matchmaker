cfcc1783cb711a5f6b0db394f35010827baab4c0
JC <jc@jcmbp.local>
2016-01-25 16:21:33 -0800

    added before block to restore db before each test

--- test/dbHelpers.js
+++ test/dbHelpers.js
@@ -1,7 +1,40 @@
 import {expect} from 'chai';
+import db from '../db/config';
+import createTables from  '../db/schemas';
 
 describe('database helpers', () => {
 	describe('getRandomUsers', () => {
+		before((done) => {
+
+			// this is done to return database to untouched state: http://stackoverflow.com/questions/3327312/drop-all-tables-in-postgresql
+			db.query('DROP SCHEMA IF EXISTS public CASCADE;');
+			.then(() => {
+				return db.query('CREATE SCHEMA public;');
+			})
+			.then(() => {
+				return db.query('GRANT ALL ON SCHEMA public TO postgres;');
+			})
+			.then(() => {
+				return db.query('GRANT ALL ON SCHEMA public TO public;');
+			})
+			.then(() => {
+				return db.query("COMMENT ON SCHEMA public IS 'standard public schema'");
+			})
+			.then(() => {
+				return createTables();
+			})
+			.then(() => {
+				console.log('tables dropped and recreated')
+				done();
+			})
+			.catch((error) => {
+				throw new Error(error);
+			});
+		});
+
+		it('should return 3 random, different users', () => {
+
+		});
 
 	});
 });
